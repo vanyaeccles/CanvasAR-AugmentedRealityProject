caa5c69b664150d8db75ba9c46f4da4732a84ebf
ITSERV\ecclesva <ecclesva@tcd.ie>
2017-03-14 10:46:06 +0000

    Initial Push for current project. Simple, draws spheres at defined target position

---
+++ ARProject/Assets/Scripts/DrawStylusScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class DrawStylusScript : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    public GameObject[] stylus;
+
+    void Start()
+    {
+        stylus = GameObject.FindGameObjectsWithTag("Stylus");
+
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Draw Stylus when target is tracked
+            foreach(GameObject stylusComp in stylus)
+            {
+                stylusComp.SetActive(true);
+            }
+            Debug.Log("Tracking Stylus");
+        }
+        else
+        {
+            foreach (GameObject stylusComp in stylus)
+            {
+                // Stop drawing Stylus when target is lost
+                stylusComp.SetActive(false);
+            }
+                
+            Debug.Log("Not Tracking Stylus");
+        }
+    }
+
+}
---
+++ ARProject/Assets/Scripts/drawer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawer : MonoBehaviour {
+
+    public GameObject drawObject;
+
+    public Transform stylusLocation;
+
+    //public Vector3 drawPos;
+
+    public Transform canvasPos;
+
+   
+
+    void Awake()
+    {
+        stylusLocation = GameObject.Find("Sphere").GetComponent<Transform>();
+
+        canvasPos = GameObject.Find("CanvasTarget").GetComponent<Transform>();
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 drawPos = new Vector3(stylusLocation.position.x, canvasPos.position.y, stylusLocation.position.z);
+
+        if (Input.GetKeyDown("space"))
+        {
+
+            Instantiate(drawObject, stylusLocation.position, stylusLocation.rotation);
+        }
+		
+	}
+}        
