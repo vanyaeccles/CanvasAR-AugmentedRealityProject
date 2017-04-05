d3794e917963de38707c226875ca04484afbdb8e
ITSERV\ecclesva <ecclesva@tcd.ie>
2017-04-05 11:20:07 +0100

    Larger targets for distance recognition

---
+++ ARProject/Assets/Scripts/FocusScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+// Sets the camera focus mode
+
+public class FocusScript : MonoBehaviour
+{
+
+    void Start()
+    {
+        bool focusModeSet = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+
+        if (!focusModeSet)
+        {
+            Debug.Log("Failed to set focus mode (unsupported mode).");
+        }
+    }
+
+}
+    
